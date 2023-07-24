0b014e7eb43cffaa89ffd80bf9aaaf8c0fe8dbcc
nikhilc01 <ncnikhil04@gmail.com>
2023-07-24 19:57:13 +0000

    terraform and variables

---
+++ vars-tf/variables.tf
@@ -0,0 +1,18 @@
+variable "container_name" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type        = string
+  default     = "ExampleNginxContainer"
+}
+variable "internal_port" {
+  description = "Value of the name for the internal portr"
+  # basic types include string, number and bool
+  type        = number
+  default     = "80"
+}
+variable "external_port" {
+  description = "Value of the name for the internal portr"
+  # basic types include string, number and bool
+  type        = number
+  default     = "8080"
+}
